c188de9e3b6b3e4eb9644887cf7560cde154b50f
igarg2001 <igarg2001@gmail.com>
2021-07-13 16:26:56 +0530

    many changes lost track

---
+++ src/components/Reusable/FourInAPageSlider.vue
@@ -0,0 +1,117 @@
+<template lang="">
+  <div id="wrapper">
+      <vueper-slides
+  class="no-shadow"
+  :visible-slides="4"
+  slide-multiple
+  :gap="3"
+  :slide-ratio="1 / 2.5"
+  :dragging-distance="200"
+  :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }">
+  <vueper-slide v-for="i in data" :key="i">
+      <template v-slot:content>
+          <div id="slideWrapper">
+              <img :src="getPic(i.image)">
+              <div id="title">{{i.title}}</div>
+              <div id="smallContent">{{i.smallContent}}</div>
+              <div id="icons">
+                <img :src="TelephoneIcon" alt="telephone" />
+                <img :src="EnvelopeIcon" alt="envelope" />
+              </div>
+          </div>
+      </template>
+  </vueper-slide>
+</vueper-slides>
+  </div>
+</template>
+<script>
+import { VueperSlides, VueperSlide } from "vueperslides";
+import TelephoneIcon from "@/assets/svg/telephone-fill.svg";
+import EnvelopeIcon from "@/assets/svg/envelope-fill.svg"
+export default {
+  created() {
+    // this.myData.map(i => console.log(typeof(i.image)))
+  },
+  name: "FourInAPageSlider",
+  components: {
+    VueperSlides,
+    VueperSlide,
+  },
+  props: ["data"],
+  data() {
+    return {
+      myData: this.data,
+    };
+  },
+  setup() {
+    return {
+      TelephoneIcon,
+      EnvelopeIcon
+    };
+  },
+  methods: {
+    getPic: (url) => {
+      return require("@/assets/images/TeamImages/" + url + "");
+    },
+  },
+};
+</script>
+<style scoped>
+#wrapper {
+  width: 94%;
+  margin-left: 3%;
+}
+#slideWrapper {
+  background: #ffffff;
+  /* Shadow0 */
+
+  box-shadow: 0px 0px 4px 2px rgba(0, 0, 0, 0.25);
+  border-radius: 2px;
+  margin-top: 5%;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 7.5% 10%;
+  /* position: relative; */
+}
+
+#slideWrapper #title {
+  font-family: "Poppins", sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 36px;
+  margin-top: 4%;
+  /* identical to box height */
+  color: #211d70;
+  text-align: center;
+}
+#slideWrapper img {
+  height: 40%;
+  width: auto;
+}
+#slideWrapper #smallContent {
+  font-family: "Poppins", sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 27px;
+  text-align: center;
+  margin-top: 4%;
+  color: #211912;
+}
+
+#slideWrapper #icons {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: absolute;
+  bottom: 10%;
+  width: 50%;
+}
+#slideWrapper #icons img {
+  width: 25px;
+  height: 25px;
+}
+</style>
--- src/pages/TeamPage.vue
+++ src/pages/TeamPage.vue
@@ -21,6 +21,19 @@
   </div>
 </template>
         </PageSection>
+        <FourInAPageSlider :data="teamData"/>
+        <PageSection>
+           <template #heading>SARC</template>
+            <template #content>
+  <div id="contentText">
+    Distinguished Alumni Awards 2020 were presented by BITS Pilani, Goa Campus
+    for the year of 2020. The event was hosted by the honorable Chancellor - Mr.
+    Kumar Mangalam Birla and Vice Chancellor - Prof. Souvik Bhattacharyya. The
+    event was well received by various prominent personalities including the
+    list of Awardees.
+  </div>
+</template>
+        </PageSection>
     </div>
     
 </template>
@@ -28,11 +41,108 @@
 <script>
 import Header from "../components/Reusable/Header.vue";
 import PageSection from "../components/Reusable/PageSection.vue";
+import FourInAPageSlider from "../components/Reusable/FourInAPageSlider.vue";
+const teamData = [
+  {
+    id: 1,
+    title: "Prof. Arya Kumar",
+    smallContent:
+      "Prof Arya Kumar is a professor in the Department of Economics and Finance, BITS Pilani. He is the Dean Alumni Relations at BITS Pilani. Prof Arya has a diverse experience of more than 40 years in the educational institutions, research organisations, Banks and Financial Institutions.",
+    fullContent: "All info about John Doe",
+    image: "Prof Arya Kumar.jpg",
+  },
+  {
+    id: 2,
+    title: "Prof. Rajeev Sakhuja",
+    smallContent:
+      "Prof. Rajeev is the Associate Prof in the Department of Chemistry. He is also the Associate Dean of BITS Pilani, Pilani campus. He joined the institute in 2012 as an Assistant Professor and in 2018, he was promoted to the position of Associate Professor. ",
+    fullContent: "All info about John Doe",
+    image: "Prof. Rajeev.jpg",
+  },
+  {
+    id: 3,
+    title: "Prof. Veeky Baths",
+    smallContent:
+      "Prof. Veeky is the Associate Prof in the Department of Biological Sciences, BITS Pilani, K.K. Birla Goa Campus. He is also the Associate Dean Alumni Relations of BITS Pilani, K.K. Birla Goa Campus. With over 15 years’ experience in teaching and research, Prof. Veeky has published various research articles in reputed journals and conferences.",
+    fullContent: "All info about John Doe",
+    image: "Prof Veeky.jpg",
+  },
+  {
+    id: 4,
+    title: "Prof. Meenakshi Viswanathan",
+    smallContent:
+      "Prof. Meenakshi is the Associate Prof in the Department of Physics at BITS Pilani, Hyderabad campus. She joined the institute in 2009 as an Assistant Professor. She is also the Associate Dean of BITS Pilani, Hyderabad campus.",
+    fullContent: "All info about John Doe",
+    image: "Prof Meenakshi.jpg",
+  },
+  {
+    id: 5,
+    title: "Prof. Trupti Gokhale",
+    smallContent:
+      "Prof. Trupti is an Associate Prof. in the Department of Biotechnology at BITS Pilani, Dubai campus. She is also the Associate Dean Alumni Relations for BITS Pilani, Dubai campus. ",
+    fullContent: "All info about John Doe",
+    image: "Prof Trupti.jpg",
+  },
+  {
+    id: 6,
+    title: "Sachin Arya",
+    smallContent:
+      "Sachin is the Head, Business Technology Incubator at PIEDS (Pilani Innovation & Entrepreneurship Development Society), BITS Pilani. He is also the Head of the Alumni Relations at BITS Pilani.",
+    fullContent: "All info about John Doe",
+    image: "Sachin.jpg",
+  },
+  {
+    id: 7,
+    title: "Raj Kumar Sharma",
+    smallContent:
+      "Raj Kumar Sharma manages the donations and the fund management of the Alumni Relations Division. ",
+    fullContent: "All info about John Doe",
+    image: "Raj.jpg",
+  },
+  {
+    id: 8,
+    title: "Saurabh Suman",
+    smallContent:
+      "Saurabh Suman is the Manager- Communications at BITS Pilani. He joined the institute in 2019.",
+    fullContent: "All info about John Doe",
+    image: "Saurabh Suman.jpg",
+  },
+  {
+    id: 9,
+    title: "Geetika",
+    smallContent:
+      "Geetika is the content writer at BITS Pilani. She joined the institute in June 2020.",
+    fullContent: "All info about John Doe",
+    image: "Geetika.jpg",
+  },
+  {
+    id: 10,
+    title: "Ramesh Saini",
+    smallContent:
+      "Ramesh Saini is the Office Assistant Database Management of the Alumni Relations Division. ",
+    fullContent: "All info about John Doe",
+    image: "Ramesh.jpg",
+  },
+  {
+    id: 11,
+    title: "Santosh Kumar Saini",
+    smallContent:
+      " Santosh Kumar Saini is the Assistant Office Superintendent.",
+    fullContent: "All info about John Doe",
+    image: "Santosh Saini.jpg",
+  },
+];
 export default {
+  data: () => {
+    return {
+      teamData,
+    };
+  },
   name: "TeamPage",
   components: {
     Header,
     PageSection,
+    FourInAPageSlider,
   },
 };
 </script>
