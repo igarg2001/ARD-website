220f1c8432921b3fa719726c765641043364ac85
Nidheesh Jain <nidheeshjain01@gmail.com>
2021-08-28 08:40:23 +0530

    gibing back basics done

---
+++ src/components/Reusable/BlackFooter.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="text">
+      <slot name="text"></slot>
+    </div>
+    <div class="buttons">
+      <slot name="buttons"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlackFooter",
+};
+</script>
+
+<style scoped>
+.container {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    background-color: #000;
+    color: #fff;
+}
+
+.text {
+    font-size: 1.2rem;
+    margin-bottom: 1rem;
+}
+
+.buttons {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+@media screen and (max-width: 780px) {
+}
+</style>
--- src/pages/GivingBackPage.vue
+++ src/pages/GivingBackPage.vue
@@ -1,165 +1,213 @@
 <template>
-  <Header :isOpaque="false" />
-  <OpaquePageTitle title="Giving Back" />
-  <PageSection style="margin-bottom: 120px;">
-    <template #heading>Your Impact</template>
-    <template #content>
-      <div class="recents">
-        
+  <div id="heroContainer">
+    <Header id="header" :isOpaque="false" />
+    <div id="heroSection">
+      <img src="../assets/GivingBack/hero.png" />
+      <div class="overlay">
+        <p>GIVING BACK</p>
       </div>
-    </template>
-  </PageSection>
-  <PageSection>
-    <template #heading>Giving to BITS</template>
-    <template #content>
-      yubvirkegjfngfnbfujbjkkj
-    </template>
-  </PageSection>
+    </div>
+  </div>
+  <div id="pageSection">
+    <PageSection>
+      <template #heading>Impact Numbers</template>
+      <template #content>
+        <div class="imapctNumberCapsulesContainer">
+          <NumberCapsule :number="'2555+'" text="Entrepreneurs" />
+          <NumberCapsule number="3500+" text="Managers" />
+          <NumberCapsule number="450+" text="Academicians" />
+          <NumberCapsule :number="'2555+'" text="Entrepreneurs" />
+          <NumberCapsule number="3500+" text="Managers" />
+          <NumberCapsule number="450+" text="Academicians" />
+        </div>
+      </template>
+    </PageSection>
+    <PageSection>
+      <template #heading>GIVING TO BITS</template>
+      <template #content>
+        <div id="contentText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim, risus
+          egestas massa pretium ultricies neque. Rutrum viverra eu bibendum
+          morbi iaculis gravida nisl.
+        </div>
+      <div id="CTAButtons">
+        <OrangeButton displayText="Alumni Giving" type="outline" :color="true" style="min-width: 20vw; text-align:center" />
+        <OrangeButton displayText="Corporate Social Responsibility" type="filled" style="min-width: 20vw; text-align:center" />
+      </div>
+      </template>
+    </PageSection>
+    <BlackFooter>
+      <template #text>
+        Wishing to donote? Get in touch!
+      </template>
+      <template #buttons>
+        <OrangeButton displayText="Call Us @99999-99999" type="outline" style="min-width: 15vw; text-align:center; margin-right: 2vw" />
+        <OrangeButton displayText="Write an E-Mail" type="filled" style="min-width: 15vw; text-align:center" />
+      </template>
+    </BlackFooter>
+
+  </div>
 </template>
 
 <script>
-import Header from "../components/Reusable/Header";
-import OpaquePageTitle from "../components/Reusable/OpaquePageTitle";
-import PageSection from "../components/Reusable/PageSection";
+import Header from "../components/Reusable/Header.vue";
+import PageSection from "../components/Reusable/PageSection.vue";
+import NumberCapsule from "@/components/Reusable/NumberCapsule";
+import OrangeButton from "@/components/Reusable/Buttons/OrangeButton";
+import BlackFooter from "@/components/Reusable/BlackFooter";
+
 export default {
+  data: () => {
+    return {};
+  },
   name: "GivingBackPage",
   components: {
     Header,
-    OpaquePageTitle,
     PageSection,
-  },
-  data() {
-    return {
-      newsletters: [
-        {
-          id: 1,
-          title: "Anecdote of BITSian Storytellers",
-          year: 2021,
-          volume: 4,
-          issue: 3,
-          imagePath: "Newsletters/echo33.png",
-        },
-        {
-          id: 2,
-          title: "An indefinite journey of BITS Pilani Entrepreneurs",
-          year: 2021,
-          volume: 4,
-          issue: 2,
-          imagePath: "Newsletters/echo44.png",
-        },
-        {
-          id: 3,
-          title: "BITSians shaping Education around the World",
-          year: 2021,
-          volume: 4,
-          issue: 1,
-          imagePath: "Newsletters/echo43.png",
-        },
-        {
-          id: 4,
-          title: "Finding opportunities in Covid-19 Crisis",
-          year: 2020,
-          volume: 3,
-          issue: 3,
-          imagePath: "Newsletters/echo42.png",
-        },
-        {
-          id: 5,
-          title: "The mind behind Amazon Alexa",
-          year: 2020,
-          volume: 3,
-          issue: 2,
-          imagePath: "Newsletters/echo40.png",
-        },
-        {
-          id: 6,
-          title: "BITS Women leading the way",
-          year: 2020,
-          volume: 3,
-          issue: 1,
-          imagePath: "Newsletters/echo41.png",
-        },
-      ],
-      pastNewsletters: [
-        {
-          id: 1,
-          title: "Anecdote of BITSian Storytellers",
-          year: 2021,
-          volume: 4,
-          issue: 3,
-          imagePath: "Newsletters/echo33.png",
-        },
-        {
-          id: 2,
-          title: "An indefinite journey of BITS Pilani Entrepreneurs",
-          year: 2021,
-          volume: 4,
-          issue: 2,
-          imagePath: "Newsletters/echo44.png",
-        },
-        {
-          id: 3,
-          title: "BITSians shaping Education around the World",
-          year: 2021,
-          volume: 4,
-          issue: 1,
-          imagePath: "Newsletters/echo43.png",
-        },
-        {
-          id: 4,
-          title: "Finding opportunities in Covid-19 Crisis",
-          year: 2020,
-          volume: 3,
-          issue: 3,
-          imagePath: "Newsletters/echo42.png",
-        },
-        {
-          id: 5,
-          title: "The mind behind Amazon Alexa",
-          year: 2020,
-          volume: 3,
-          issue: 2,
-          imagePath: "Newsletters/echo40.png",
-        },
-        {
-          id: 6,
-          title: "BITS Women leading the way",
-          year: 2020,
-          volume: 3,
-          issue: 1,
-          imagePath: "Newsletters/echo41.png",
-        },
-      ],
-    };
+    NumberCapsule,
+    OrangeButton,
+    BlackFooter
   },
 };
 </script>
-
 <style scoped>
-.recents {
+#heroContainer {
+  position: relative;
+  min-height: 25vw;
+}
+#header {
+  position: absolute;
+  top: 0;
+}
+#heroSection {
+  position: absolute;
+  top: 0;
+  z-index: -1;
+  width: 100vw;
+}
+#heroSection img {
+  height: 100%;
+  width: 100%;
+
+  object-fit: cover;
+}
+
+#heroSection .overlay {
+  position: absolute;
+  background: rgba(8, 7, 10, 0.8);
+  height: 99%;
+  width: 100%;
+  top: 0;
   display: flex;
-  flex-direction: row;
   justify-content: center;
   align-items: center;
-  flex-wrap: wrap;
-  max-width: 100vw;
 }
-
-.recents > div {
-  margin: 0px 4%;
-  margin-bottom: 80px;
-}
-
-.pastRelease {
-  text-decoration: none;
-  font-size: 1.3em;
-  margin-bottom: 40px;
+#heroSection .overlay p {
+  font-family: "Maven Pro", sans-serif;
+  font-size: 3vw;
+  color: white;
+  font-weight: 900;
+  white-space: nowrap;
   text-align: center;
-  line-height: 0.96;
-  color: #21244F;
+  margin-top: 4vh;
 }
 
-.pastRelease:hover {
-  text-decoration: underline;
+#contentText {
+  width: 85%;
+  color: #08070a;
+
+  /* text white shadow */
+  text-shadow: 0px 4px 60px #ffffff;
+  font-family: "Baloo 2", sans-serif;
+  font-size: 23px;
+  text-align: center;
+}
+
+.imapctNumberCapsulesContainer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-bottom: -2vw;
+}
+
+.imapctNumberCapsulesContainer > div {
+  margin-right: 2vw;
+  margin-left: 2vw;
+  margin-bottom: 2vw;
+}
+
+
+#CTAButtons {
+  margin-top:50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+#CTAButtons > div {
+  padding: 15px 40px;
+  font-size: 19px;
+}
+
+#CTAButtons div:first-child {
+  margin-right: 60px;
+}
+
+
+@media screen and (max-width: 768px) {
+  #heroContainer {
+    min-height: 50vh;
+  }
+  #heroSection {
+    height: 100%;
+  }
+  #heroSection img {
+    height: 100%;
+  }
+  #heroSection .overlay {
+    height: 100%;
+  }
+
+  #heroSection .overlay p {
+    font-size: 6.5vw;
+  }
+  #contentText {
+    font-size: 17px;
+  }
+
+  .imapctNumberCapsulesContainer {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    margin-bottom: 0vw;
+  }
+
+  .imapctNumberCapsulesContainer > div {
+    margin-bottom: 20px;
+    margin-right: 0px;
+  }
+
+  .imapctNumberCapsulesContainer > div:last-child {
+    margin-bottom: 0px;
+  }
+
+  
+  #CTAButtons {
+    flex-direction: column-reverse;
+  }
+
+  #CTAButtons > div {
+    font-size: 4vw;
+    width: 90vw;
+  }
+
+  #CTAButtons div:first-child {
+    margin-top: 20px;
+    margin-right: 0;
+  }
 }
 </style>
