02e7d30531fc0890e0d0b5dcc93c690cbaa18097
igarg2001 <igarg2001@gmail.com>
2022-03-29 17:27:14 +0530

    font preloading

--- src/App.vue
+++ src/App.vue
@@ -13,18 +13,21 @@ export default {
 
 <style>
 @font-face {
+  font-display: auto;
   font-family: "Urby";
   src: url('~@/fonts/Urby Regular.otf');
   font-style: normal;
   font-weight: 400;
 }
 @font-face {
+  font-display: auto;
   font-family: "Urby";
   src: url('~@/fonts/Urby Black.otf');
   font-style: normal;
   font-weight: 900;
 }
 @font-face {
+  font-display: auto;
   font-family: "Urby";
   src: url('~@/fonts/Urby Bold.otf');
   font-style: normal;
@@ -32,36 +35,42 @@ export default {
 }
 
 @font-face {
+  font-display: auto;
   font-family: 'AmsiProNarw';
   src: url('~@/fonts/AmsiProNarw-Regular.otf');
   font-style: normal;
   font-weight: 400;
 }
 @font-face {
+  font-display: auto;
   font-family: 'AmsiProNarw';
   src: url('~@/fonts/AmsiProNarw-Light.otf');
   font-style: normal;
   font-weight: 200;
 }
 @font-face {
+  font-display: auto;
   font-family: 'AmsiProNarw';
   src: url('~@/fonts/AmsiProNarw-Bold.otf');
   font-style: normal;
   font-weight: 600;
 }
 @font-face {
+  font-display: auto;
   font-family: "AmsiProCond";
   src: url('~@/fonts/AmsiProCond-Light.otf');
   font-style: normal;
   font-weight: 300;
 }
 @font-face {
+  font-display: auto;
   font-family: "AmsiPro";
   src: url('~@/fonts/AmsiPro-Black.otf');
   font-style: normal;
   font-weight: 900;
 }
 #app {
+  font-display: auto;
   font-family: "Baloo 2", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
